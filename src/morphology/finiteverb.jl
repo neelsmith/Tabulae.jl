9267a18c31211654f5733cd3b2d6fa271e6cf5d8
Neel Smith <dnsmith.neel@gmail.com>
2021-08-05 00:02:52 -0400

    Work on reading verb FST output

--- src/morphology/finiteverb.jl
+++ src/morphology/finiteverb.jl
@@ -17,8 +17,37 @@ end
 
 $(SIGNATURES)
 """
-function verbfromfst(s)
+function verbfromfst(fstdata)
+
+    # Example rule string:
+    #  "<w_regular><finiteverb>ει<third><singular><present><indicative><active>"
+    # Extract PNTMV from a string like the example:
+    verbrulere = r"<[^<]+><finiteverb>[^<]*<([^<]+)><([^<]+)><([^<]+)><([^<]+)><([^<]+)>"
+    matchedup = collect(eachmatch(verbrulere, fstdata))
+
+    if isempty(matchedup)
+        @warn("Unable to parse FST analysis \"" * fstdata * "\" as verb form.")
+        nothing
+    else
+        (p,n, t, m, v) = matchedup[1].captures
+
+        persondict = labeldict(personpairs)
+        numberdict = labeldict(numberpairs)
+        tensedict = labeldict(tensepairs)
+        mooddict = labeldict(moodpairs)
+        voicedict = labeldict(voicepairs)
+        verbform = FiniteVerbForm(persondict[p], p,
+        numberdict[n], n,
+        tensedict[t], t,
+        mooddict[m], m,
+        voicedict[v], v
+        )
+
+        FormUrn(string("morphforms.", FINITEVERB,verbform.vperson, verbform.vnumber, 
+        verbform.vtense, verbform.vmood, verbform.vvoice, "000"))
+    end
 end
+
 
 #abduxit
 #<u>latcommon.compoundn55_0</u><u>ls.n55</u><#>abdux<verb><pftact><div><pftact><verb>it<3rd><sg><pft><indic><act><u>latcommon.pftact_pft3</u>
--- src/serialization.jl
+++ src/serialization.jl
@@ -1,5 +1,14 @@
 
+function functionforform()
+    Dict(
+        "verb" =>  Tabulae.verbfromfst
+    )
+end
 
+"""Parse a line of SFST output into a `LatinMorphologicalForm`.
+
+$(SIGNATURES)
+"""
 function fromfst(s)
 
     (stemraw,rule) = split(s, "<div>")
@@ -11,15 +20,16 @@ function fromfst(s)
     stemmatch = collect(eachmatch(stemre, stem))
     (stemidval, lexidval, tkn, analysiscat, stemdata) = stemmatch[1].captures
     
-    
-    #=
+ 
     rulere = r"(<[^>]+><[^>]+>)([^<]*)(.*)<u>(.+)</u>"
     rulematch = collect(eachmatch(rulere, rule))
     (typeinfo, ending, ruledata, ruleidval) = rulematch[1].captures
 
 
-    fnctndict = functionfollowsform()
+    fnctndict = functionforform()
     fnct = fnctndict[analysiscat]
+
+    
     # Depends on what is regular, what is irregular!
     formurn = ""
     if analysiscat == "irregular"
@@ -29,7 +39,7 @@ function fromfst(s)
     end
     
     Analysis(string(tkn,ending), LexemeUrn(lexidval), formurn, RuleUrn(ruleidval), StemUrn(stemidval))
-=#
+
 end
 
 # <u>latcommon.nounn305</u><u>ls.n305</u>acast<noun><masc><us_i><div><us_i><noun>i<masc><nom><pl><u>livymorph.us_i6</u>
