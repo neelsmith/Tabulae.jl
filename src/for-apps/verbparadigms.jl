917910a6efcc028587d9a1884ed3acf899b8bb8d
Neel Smith <dnsmith.neel@gmail.com>
2022-04-23 07:09:23 -0400

    Fix display of subj.pass.

--- src/for-apps/verbparadigms.jl
+++ src/for-apps/verbparadigms.jl
@@ -1,4 +1,28 @@
+function cyclethem(t::LMPTense, lex::LexemeUrn, td::Tabulae.Dataset)
+    tenseforms = filter(f -> f.vtense == t, finiteverbforms())
+    println("IND ACT")
+    for i in 1:3
+        println(label(tenseforms[i]))
+    end
+    println("SUBJ ACT")
+    for i in 7:9
+        println(label(tenseforms[i]))
+    end
 
+    println("IND PASS")
+    for i in 13:15
+        println(label(tenseforms[i]))
+    end
+    println("SUBJ PASS")
+    for i in 19:21
+        println(label(tenseforms[i]))
+    end
+
+end
+
+"""Format markdown to display conjugational paradigm of a single tense system of `lex`.
+$(SIGNATURES)
+"""
 function md_tenseconjugation(t::LMPTense, lex::LexemeUrn, td::Tabulae.Dataset)
     tenseforms = filter(f -> f.vtense == t, finiteverbforms())
     
@@ -35,21 +59,21 @@ function md_tenseconjugation(t::LMPTense, lex::LexemeUrn, td::Tabulae.Dataset)
         push!(mdlines, "")
         push!(mdlines, "| | Indicative | Subjunctive |")
         push!(mdlines, "| --- | --- | --- |")
-        for i in 7:9
-            subj = tenseforms[i]
-            subjunctive = CitableParserBuilder.tokens( generate(subj,lex,td))
-
-            ind = tenseforms[i + 6]
+        for i in 13:15
+            ind = tenseforms[i]
             indicative = CitableParserBuilder.tokens( generate(ind,lex,td))
+
+            subj = tenseforms[i + 6]
+            subjunctive = CitableParserBuilder.tokens( generate(subj,lex,td))
             rowheader = join([label(lmpPerson(ind)), label(lmpNumber(ind)) ], " ")
-            push!(mdlines, "| **$(rowheader)** | $(indicative) | $(subjunctive) | ")
+            push!(mdlines, "| **$(rowheader)** | $(indicative) |  $(subjunctive) | ")
         end
         for i in 10:12
-            subj = tenseforms[i]
-            subjunctive = CitableParserBuilder.tokens( generate(subj,lex,td))
-
-            ind = tenseforms[i + 6]
+            ind = tenseforms[i]
             indicative = CitableParserBuilder.tokens( generate(ind,lex,td))
+
+            subj = tenseforms[i + 6]
+            subjunctive = CitableParserBuilder.tokens( generate(subj,lex,td))
             rowheader = join([label(lmpPerson(ind)), label(lmpNumber(ind)) ], " ")
             push!(mdlines, "| **$(rowheader)** | $(indicative) | $(subjunctive) | ")
         end
