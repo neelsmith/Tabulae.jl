7ff1bbb17b04c0f1b04d2b1f28becaa0e23ec0a8
Neel Smith <dnsmith.neel@gmail.com>
2024-06-06 06:26:10 -0400

    Clear out old dods dir

--- docs/make.jl
+++ docs/make.jl
@@ -19,42 +19,10 @@ makedocs(
     sitename="Tabulae.jl",
     pages = [
         "Home" => Any[
-            "Quick start" => "quick.md",
-            "User's guide: introduction" => "index.md",
-            "Datasets" => "guide/datasets.md",
-            "Identification with URNs" => "guide/urns.md",
-            "Vocabulary" => "guide/lexemes.md",
-            "Morphological forms" => "guide/forms.md",
-            "Rules data" => "guide/rules.md",
-            "Stems data" => "guide/stems.md",
-            "Analyses" => "guide/analyses.md",
-
-        ],
-        "Reference: inflectional types and paradigms" => Any[
-            "Paradigms" =>  "infltypes/index.md",
-            "Nouns" => "infltypes/nouns/index.md",
-            "Nouns: first declension" => "infltypes/nouns/decl1.md",
-            "Nouns: second declension" => "infltypes/nouns/decl2.md",
-            "Nouns: third declension" => "infltypes/nouns/decl3.md",
-            "Nouns: fourth declension" => "infltypes/nouns/decl4.md",
-            "Nouns: fifth declension" => "infltypes/nouns/decl5.md",
-
-            "Verbs" => "infltypes/verbs/index.md",
-            "Verbs: first conjugation" => "infltypes/verbs/conj1.md",
     
-            "Verbs: second conjugation" => "infltypes/verbs/conj2.md",
-            "Verbs: third conjugation" => "infltypes/verbs/conj3.md",
-            "Verbs: third-io conjugation" => "infltypes/verbs/conj3io.md",
-            "Verbs: fourth conjugation" => "infltypes/verbs/conj4.md",
-            "Verbs with irreglar principal parts" => "infltypes/verbs/princparts.md"
+            "Documentation" => "index.md",
 
-        ],
-
-        "Reference: code" => Any[
-            "Latin Morphology API" =>  "apis/apis.md"
-           
-        ],
-        
+        ],  
     ],
     )
 
--- docs/src/index.md
+++ docs/src/index.md
@@ -1,7 +1,4 @@
 # Tabulae.jl
 
 
-
-
-
 Documentation is now on quarto.pub, [here](https://neelsmith.quarto.pub/tabulae/).
