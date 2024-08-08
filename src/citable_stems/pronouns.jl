8fbada3e384bf06ccd709a30862eb3139a815f01
Neel Smith <dnsmith.neel@gmail.com>
2024-08-08 11:21:01 -0400

    Test pronoun forms

---
+++ src/citable_stems/pronouns.jl
@@ -0,0 +1,155 @@
+"A record for a single noun stem."
+struct TabulaePronounStem <: TabulaeStem
+    stemid::StemUrn
+    lexid::LexemeUrn
+    form::AbstractString
+    gender::LMPGender
+    inflectionclass::AbstractString
+end
+
+"""Override Base.show for noun stem type.
+$(SIGNATURES)
+"""
+function show(io::IO, noun::TabulaePronounStem)
+    print(io, label(noun))
+end
+
+"""Override Base.== for pronoun stem type.
+$(SIGNATURES)
+"""
+function ==(pn1::TabulaePronounStem, pn2::TabulaePronounStem)
+    id(pn1) == id(pn2) &&
+    lexeme(pn1) == lexeme(pn2) &&
+    stemvalue(pn1) == stemvalue(pn2)  &&
+    lmpGender(pn1) == lmpGender(pn2) &&
+
+    inflectionclass(pn1) == inflectionclass(pn2)
+
+end
+
+
+CitableTrait(::Type{TabulaePronounStem}) = CitableByCite2Urn()
+"""Pronoun stems are citable by Cite2Urn.
+$(SIGNATURES)
+"""
+function citabletrait(::Type{TabulaePronounStem})
+    CitableByCite2Urn()
+end
+
+"""Human-readlable label for a `TabulaePronounStem`.
+
+$(SIGNATURES)
+Required for `CitableTrait`.
+"""
+function label(pns::TabulaePronounStem)
+    string("Pronoun stem ", pns.form, "- (", label(pns.gender), ")")
+end
+
+
+"""Identifying URN for a `TabulaePronounStem`.  If
+no registry is included, use abbreviated URN;
+otherwise, expand to full `Cite2Urn`.
+
+$(SIGNATURES)
+Required for `CitableTrait`.
+"""
+function urn(pns::TabulaePronounStem; registry = nothing)
+    if isnothing(registry)
+        pns.stemid
+    else
+        expand(pns.stemid, registry)
+    end
+end
+
+
+
+struct TabulaePronounStemCex <: CexTrait end
+"""Pronoun stems are are CEX serializable.
+$(SIGNATURES)
+"""
+function cextrait(::Type{TabulaePronounStem})  
+    TabulaePronounStemCex()
+end
+
+"""Compose CEX text for a `TabulaePronounStem`.
+If `registry` is nothing, use abbreivated URN;
+otherwise, expand identifier to full `Cite2Urn`.
+$(SIGNATURES)
+"""
+function cex(pns::TabulaePronounStem; delimiter = "|", registry = nothing)
+    if isnothing(registry)
+        join([id(pns), lexeme(pns), stemvalue(pns),
+        label(lmpGender(pns)),
+        inflectionclass(pns)
+        ], delimiter)
+    else
+        c2urn = expand(id(pns), registry)
+        join([c2urn,  lexeme(pns), stemvalue(pns),
+        label(lmpGender(pns)),
+        inflectionclass(pns)], delimiter)
+    end
+end
+
+
+
+"""Instantiate a pronoun stem from delimited-text source.
+$(SIGNATURES)
+"""
+function fromcex(traitvalue::TabulaePronounStemCex, cexsrc::AbstractString, T;      
+    delimiter = "|", configuration = nothing, strict = true)
+    parts = split(cexsrc, delimiter)
+
+    @debug("Parts: $(parts)")
+    if length(parts) < 5
+        throw(DomainError("Too few parts in $(delimited)"))
+    else
+        stemid = StemUrn(parts[1])
+        lexid = LexemeUrn(parts[2])
+        stem = parts[3]
+        @debug("Look at parts4 $(parts[4])")
+        gender = lmpGender(parts[4])
+        inflclass = parts[5]
+
+        @debug("Made gender object $(gender)")
+        TabulaePronounStem(stemid,lexid,stem,gender,inflclass)
+    end
+end
+
+"""Identifier for a `TabulaePronounStem`, as an
+abbreviated URN.
+
+$(SIGNATURES)
+"""
+function id(pn::TabulaePronounStem)
+    pn.stemid
+end
+
+"""Lexeme for a `TabulaePronounStem`, as an 
+abbreviated URN.
+
+$(SIGNATURES)
+"""
+function lexeme(pn::TabulaePronounStem)
+    pn.lexid
+end
+
+"""Gender for a `TabulaePronounStem`
+$(SIGNATURES)
+"""
+function lmpGender(pn::TabulaePronounStem)
+    pn.gender
+end
+
+"""Identify inflection type for `noun`.
+$(SIGNATURES)
+"""
+function inflectionclass(pronoun::TabulaePronounStem)
+    pronoun.inflectionclass
+end
+
+"""Identify stem string for `pronoun`.
+$(SIGNATURES)
+"""
+function stemvalue(pronoun::TabulaePronounStem)
+    pronoun.form
+end
---
+++ test/forms/test_pronoun.jl
@@ -0,0 +1,31 @@
+
+@testset "Test pronoun forms constructor and accessors" begin
+    pronoun = LMFPronoun(lmpGender("masculine"), lmpCase("accusative"), lmpNumber("singular"))
+    @test pronoun == LMFPronoun(LMPGender(1), LMPCase(4), LMPNumber(1))
+
+    @test label(pronoun) == "masculine accusative singular" 
+    @test urn(pronoun) == Cite2Urn("urn:cite2:tabulae:forms.v1:8010001400")
+    @test cex(pronoun) == "urn:cite2:tabulae:forms.v1:8010001400|masculine accusative singular"
+    @test lmfPronoun(urn(pronoun)) == pronoun
+
+    #=
+    @test pronoun == lmfPronoun("8010001400")
+    
+
+    frm = FormUrn("forms.8010001400")
+    @test pronoun == lmfNoun(frm)
+    @test Tabulae.formurn(pronoun) == frm
+
+    @test lmpGender(pronoun) == lmpGender("masculine")
+    @test lmpCase(pronoun) == lmpCase("accusative")
+    @test lmpNumber(pronoun) == lmpNumber("singular")
+    =#
+
+end
+
+@testset "Test CITE interface on pronouns" begin
+    pronoun = LMFPronoun(lmpGender("masculine"), lmpCase("accusative"), lmpNumber("singular")) 
+    @test citable(pronoun)
+    @test label(pronoun) == "masculine accusative singular"
+    @test urn(pronoun) == Cite2Urn("urn:cite2:tabulae:forms.v1:8010001400")
+end
