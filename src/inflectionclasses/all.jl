4ed7cc9b7ad56364971496b108eec8dbffe1e378
Neel Smith <dnsmith.neel@gmail.com>
2025-01-23 06:07:06 -0500

    Add noun inflectionclasses

--- src/inflectionclasses/all.jl
+++ src/inflectionclasses/all.jl
@@ -1,3 +1,3 @@
 function inflectionclasses()
-    nounclasses
+    nounclasses()
 end
--- src/inflectionclasses/nounclasses.jl
+++ src/inflectionclasses/nounclasses.jl
@@ -1,8 +1,8 @@
 decl1 = [a_ae, a_ae_pl]
 decl2 = [us_i]
 decl3 = [en_inis, null_is]
-decl4 = []
-decl5 = []
+decl4 = [us_us]
+decl5 = [es_ei]
 
 function nounclasses()
     vcat(decl1, decl2, decl3, decl4, decl5)
