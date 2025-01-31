2859ce9dd40dac1ce61f198c94c5c60426f3d4fb
Neel Smith <dnsmith.neel@gmail.com>
2025-01-31 07:26:07 -0500

    Develop data sets

---
+++ scripts/buildaugustine.jl
@@ -0,0 +1,61 @@
+# Build parser in Latin 24 orthography, including Lewis-Short with custom additions for Augustine, Confessions.
+using Tabulae
+using CitableParserBuilder
+
+# Root directory of Tabulae repo:
+reporoot = pwd()
+# Root directory of LexiconMining repo:
+lmroot = joinpath(dirname(pwd()), "LexiconMining.jl")
+dsroot = joinpath(lmroot, "tabulae-datasets", "lewis-short")
+isdir(dsroot)
+
+
+using Dates
+function getdatestr()
+    datesrc = now() 
+    yr = datesrc |> year
+    mo = datesrc |> month
+    monthday = datesrc |> day
+    join([yr,mo,monthday], "-")
+end
+datestr = getdatestr()
+
+
+
+
+# Names of output files:
+outfile = joinpath(reporoot, "scratch", "confessions-$(datestr).cex")
+currfile = joinpath(reporoot, "scratch", "confessions-current.cex")
+
+
+
+function confessionsdirlist(tabulaeroot, lexmineroot)
+    # Tabulae core datasets:
+    inflcommon = joinpath(tabulaeroot, "datasets", "core-infl-shared")
+    vocabcommon = joinpath(tabulaeroot, "datasets", "core-vocab-shared")
+    infl24 = joinpath(tabulaeroot, "datasets", "core-infl-lat24")
+    vocab24 = joinpath(tabulaeroot, "datasets", "core-vocab-lat24")
+
+    tailored = joinpath(tabulaeroot, "datasets", "confessions")
+
+    # Tabulae manually vetted vocabulary:
+    manualcommon = joinpath(tabulaeroot, "datasets", "manual", "common")
+
+    # Lewis-Short auto-generated datasets:
+    lscommon = joinpath(lexmineroot, "common")
+    ls24 = joinpath(lexmineroot, "lat24")
+    [inflcommon, vocabcommon, infl24, vocab24, manualcommon, lscommon, ls24 , tailored] 
+end
+confessionsdirs = confessionsdirlist(reporoot, dsroot)
+
+
+# Parser:
+function reparse()
+    parser = dataset(confessionsdirs) |> tabulaeStringParser
+    tofile(parser, outfile)
+    tofile(parser, currfile)
+end
+@time reparse()
+parsetoken("Christus",parser)
+
+
