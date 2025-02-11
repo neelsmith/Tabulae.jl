eb3cfb71aad05b0106ceb42938f17daaf6c955f1
Documenter.jl <documenter@juliadocs.github.io>
2025-02-11 20:30:25 +0000

    build based on a175151

---
+++ v0.16.6/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#Tabulae.jl","page":"Documentation","title":"Tabulae.jl","text":"","category":"section"},{"location":"","page":"Documentation","title":"Documentation","text":"Documentation is now on quarto.pub, here.","category":"page"}]
+}
